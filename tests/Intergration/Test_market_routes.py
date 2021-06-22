870539fde1230d29cdb62ac7b98b3f6991841982
Mihlali-max <you@example.com>
2021-06-22 12:32:46 +0200

    update

---
+++ tests/Intergration/Test_market_routes.py
@@ -0,0 +1,85 @@
+from flask import request
+
+from tests.base_test import BaseTest
+from market import db
+from market.models import User, Item
+
+
+class TestMarketRoute(BaseTest):
+    def test_market_route_can_purchase(self):
+        with self.app:
+            with self.app_context:
+                response = self.app.post('/register',
+                                         data=dict(username="MihlaliM",
+                                                   email_address="momozamihla@gmail.com",
+                                                   password1="alumna77", password2="alumna77",), follow_redirects=True)
+
+                user = db.session.query(User).filter_by(email_address="momozamihla@gmail.com").first()
+                self.assertTrue(user)
+
+                response = self.app.post('/login', data=dict(username="MihlaliM", password="alumna77"), follow_redirects=True)
+
+                self.assertIn(b'Success! You are logged in as: JoeDoe', response.data)
+
+                # Asserting that the user is redirected to the market page after login
+                self.assertEqual('http://localhost/market', request.url)
+
+                # no need for a post request because the user is redirected to the market route automatically
+                market_post_request = self.app.post('/market', follow_redirects=True)
+                self.assertTrue(market_post_request.status_code, 200)
+
+                purchased_item = Item(id=1, name="New Product",
+                                      price=300,
+                                      barcode=876878,
+                                      description="Fresh form the box")
+
+                purchased_item1 = Item(id=1, name="New Product",
+                                       price=3000,
+                                       barcode=876878,
+                                       description="Fresh form the box")
+
+                self.assertTrue(user.can_purchase(purchased_item))
+                self.assertFalse(user.can_purchase(purchased_item1))
+
+    def test_market_route_can_sell(self):
+        with self.app:
+            with self.app_context:
+                response = self.app.post('/register',
+                                         data=dict(id=1, username="MihlaliM", email_address="momozamihla@gmail.com",
+                                                   password1="alumna77", password2="alumna77",), follow_redirects=True)
+
+                user = db.session.query(User).filter_by(email_address="momozamihla@gmail.com").first()
+                self.assertTrue(user)
+
+                response1 = self.app.post('/register',
+                                          data=dict(id=2, username="MihlaliM", email_address="momozamihla@gmail.com",
+                                                    password1="202176", password2="202176",), follow_redirects=True)
+
+                user1 = db.session.query(User).filter_by(email_address="momozamihla@gmail.com").first()
+                self.assertTrue(user1)
+
+                # Asserting that the user is redirected to the market page after login
+                self.assertEqual('http://localhost/market', request.url)
+
+                # no need for a post request because the user is redirected to the market route automatically
+                # market_post_request = self.app.post('/market', follow_redirects=True)
+                # self.assertTrue(market_post_request.status_code, 200)
+
+                purchased_item = Item(id=1, name="New Product", price=200, barcode=876878,
+                                      description="Fresh from the box", owner=1)
+                db.session.add(purchased_item)
+                db.session.commit()
+
+                self.assertTrue(user.can_sell(purchased_item))
+                self.assertFalse(user1.can_sell(purchased_item))
+
+    def test_market_route_returns_market(self):
+        with self.app:
+            with self.app_context:
+                response1 = self.app.post('/register',
+                                          data=dict(id=2, username="MihlaliM", email_address="momozamihla@gmail.com",
+                                                    password1="aluki", password2="aluki",), follow_redirects=True)
+
+                response = self.app.post('/market', follow_redirects=True)
+
+                self.assertEqual('http://localhost/market', request.url)
--- tests/Intergration/routes_tests.py
+++ tests/Intergration/routes_tests.py
@@ -1,19 +1,28 @@
 from unittest import TestCase
 from tests.base_test import BaseTest
 from flask import request
-from tests.base_test import BaseTest
+
 from flask import request
-from market import db
-from market.models import User, Item
+from tests.base_test import BaseTest
 
-class Testing_routes(BaseTest):
 
-    def test_home_route(self):
+class TestHomeRoute(BaseTest):
+    def test_home(self):
         with self.app:
             with self.app_context:
-                response = self.app.get('/home', follow_redirects=True)
-                self.assertEqual('http://localhost/home',request.url)
-                self.assertEqual(response.status_code, 200)
+                    response = self.app.get('/home', follow_redirects=True)
+
+                    self.assertEqual('http://localhost/home', request.url)
+                    self.assertIn(b'Start purchasing products by clicking the link below', response.data)
+                    self.assertTrue(response.status_code, 200)
+
+    def test_route(self):
+        with self.app:
+                with self.app_context:
+                    response = self.app.get('/', follow_redirects=True)
+
+                    self.assertEqual('http://localhost/', request.url)
+                    self.assertIn(b'Start purchasing products by clicking the link below', response.data)
 
     def test_market_route_get(self):
         with self.app:
@@ -33,16 +42,16 @@ class Testing_routes(BaseTest):
                 response = self.app.get('/login', follow_redirects=True)
                 self.assertEqual('http://localhost/login', request.url)
                 self.assertEqual(response.status_code, 200)
-
-    def test_market_route_(self):
-        with self.app:
-            budget_variable = self.app.post(username='MihlaliM', email_address='momozamihlali@gmail.com', budget=4000)
-            self.assertEqual(budget_variable.budget, 4000)
-
-    def test_Item_Purchase(self):
-        Item_Info=Item(id=1 ,name ='Iphone2',price=2000,barcode='908098',description='latest model',owner='Mihlali')
-        purchase=User(username='MihlaliM',password_hash='MihlaliM',email_address='momozamihlali@gmail.com',budget=3000).can_purchase(Item_Info)
-        self.assertTrue(purchase)
+    #
+    # def test_market_route_(self):
+    #     with self.app:
+    #         budget_variable = self.app.post(username='MihlaliM', email_address='momozamihlali@gmail.com', budget=4000)
+    #         self.assertEqual(budget_variable.budget, 4000)
+    #
+    # def test_Item_Purchase(self):
+    #     Item_Info=Item(id=1 ,name ='Iphone2',price=2000,barcode='908098',description='latest model',owner='Mihlali')
+    #     purchase=User(username='MihlaliM',password_hash='MihlaliM',email_address='momozamihlali@gmail.com',budget=3000).can_purchase(Item_Info)
+    #     self.assertTrue(purchase)
 
 
 
